a10c24eb9eb80a326c334c60c2ac1d61c8ef03cb
Anderson <soaresanderson221@gmail.com>
2026-01-04 12:23:04 +0100

    Increeasing rate limits

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.config['ENVIRONMENT_FOLDER'] = 'data/environment'
 limiter = Limiter(
     app=app,
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["10000 per day", "1000 per hour"],
     storage_uri="memory://",
     strategy="fixed-window"
 )
